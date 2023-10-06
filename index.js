01d4725405e1af4e68339c2ac0bfd0ca313111a8
TheJoacoCo <joaquin.xibilia@gmail.com>
2023-10-05 22:17:40 -0300

    Entrega Final

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Array de profesiones médicas con horas semanales promedio
-    const profesionesMedicas = [
-        { nombre: "Médico General", horasSemanales: 40 },
-        { nombre: "Cirujano", horasSemanales: 50 },
-        { nombre: "Pediatra", horasSemanales: 35 },
-        { nombre: "Dermatólogo", horasSemanales: 38 },
-        { nombre: "Ginecólogo", horasSemanales: 42 },
-        { nombre: "Oftalmólogo", horasSemanales: 37 }
-    ];
-
     // Obtener el elemento de selección de profesión
     const profesionSelect = document.getElementById("profesion");
+    let profesionesMedicas = []; // Definir la variable en un alcance más amplio
+    let canvas; // Declarar la variable canvas en un alcance más amplio
 
-    // Cargar la profesión preferida del usuario desde el almacenamiento local
-    const profesionPreferida = localStorage.getItem("profesionPreferida");
-    
-    // Si se encuentra una profesión preferida en el almacenamiento local, establecerla como seleccionada
-    if (profesionPreferida) {
-        profesionSelect.value = profesionPreferida;
-    }
+    // Cargar los datos desde el archivo JSON local
+    fetch("./data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Obtener la lista de profesiones desde los datos cargados
+            profesionesMedicas = data.profesiones; // Asignar los datos al arreglo de profesiones
 
+            // Llenar el elemento select con las opciones de profesiones
+            profesionesMedicas.forEach((profesion) => {
+                const option = document.createElement("option");
+                option.value = profesion.nombre;
+                option.textContent = profesion.nombre;
+                profesionSelect.appendChild(option);
+            });
+
+            // Cargar la profesión preferida del usuario desde el almacenamiento local
+            const profesionPreferida = localStorage.getItem("profesionPreferida");
+
+            // Si se encuentra una profesión preferida en el almacenamiento local, establecerla como seleccionada
+            if (profesionPreferida) {
+                profesionSelect.value = profesionPreferida;
+            }
+        })
+        .catch((error) => {
+            console.error("Error al cargar el archivo JSON: " + error);
+        });
+
+    // Función para calcular el sueldo
     function calcularSueldo() {
         // Obtener los valores de los campos de entrada
         const nombre = document.getElementById("nombre").value;
@@ -33,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Validar la elección del usuario
         if (opcionProfesion) {
-            const profesionElegida = profesionesMedicas.find(profesion => profesion.nombre === opcionProfesion); 
+            const profesionElegida = profesionesMedicas.find(profesion => profesion.nombre === opcionProfesion);
             const sueldoMensual = montoPorHora * profesionElegida.horasSemanales * 4;
             const sueldoAnual = sueldoMensual * 12;
 
@@ -52,6 +64,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
             // Guardar la profesión preferida del usuario en el almacenamiento local
             localStorage.setItem("profesionPreferida", opcionProfesion);
+
+            // Crear y mostrar el gráfico
+            crearGrafico(sueldoAnualConInflacion);
         } else {
             alert("Opción no válida. Por favor, elija una opción válida.");
         }
@@ -60,4 +75,56 @@ document.addEventListener("DOMContentLoaded", function () {
     // Encuentra el botón por su id y agrega un event listener para el clic
     const calcularButton = document.getElementById("calcularButton");
     calcularButton.addEventListener("click", calcularSueldo);
+
+    // Encuentra el botón "Guardar Profesión Favorita" y agrega un event listener para el clic
+    const guardarProfesionButton = document.getElementById("guardarProfesion");
+    guardarProfesionButton.addEventListener("click", function () {
+        const profesionPreferida = profesionSelect.value;
+        localStorage.setItem("profesionPreferida", profesionPreferida);
+        alert(`Profesión ${profesionPreferida} guardada como favorita.`);
+    });
+
+    // Función para crear y mostrar el gráfico
+    function crearGrafico(sueldoAnualConInflacion) {
+        // Encuentra el elemento del canvas donde se dibujará el gráfico
+        canvas = document.getElementById("graficoSueldo");
+        const containerWidth = 600;
+        const canvasWidth = containerWidth;
+        const canvasHeight = (containerWidth * 2) / 3;
+
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        canvas.width = 400;
+        canvas.height = 300;
+
+        const ctx = canvas.getContext("2d");
+        const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+        const sueldosMensuales = Array.from({ length: 12 }, (_, i) => sueldoAnualConInflacion / 12);
+
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: meses,
+                datasets: [{
+                    label: 'Sueldo Mensual con Inflación',
+                    data: sueldosMensuales,
+                    backgroundColor: 'rgba(0, 120, 212, 0.6)', // Color de las barras
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'Sueldo Mensual ($)'
+                        }
+                    }
+                }
+            }
+        });
+    }
 });
